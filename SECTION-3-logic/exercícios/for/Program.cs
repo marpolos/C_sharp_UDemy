3f6ed42cd0f1287e4f69df424ff66b17d56f0db1
Marcelle Monteiro <monteiro.bio@outlook.com>
2023-04-03 21:40:11 -0300

    for program

---
+++ SECTION-3-logic/exercícios/for/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace exercicios;
+
+public class ExercicioFor
+{
+    public static void Main()
+    {
+        SumNumbers();
+    }
+
+    public static void SumNumbers()
+    {
+        Console.Write("Quandos números inteiros você vai digitar? ");
+        int totalNumber = Int32.Parse(Console.ReadLine());
+        List<int> numbers = new();
+        int sum = 0;
+
+        for (int i = 1; i <= totalNumber; i++)
+        {
+            Console.Write($"Digite o #{i}: ");
+            int n = Int32.Parse(Console.ReadLine());
+            numbers.Add(n);
+            sum += n;
+        }
+        Console.WriteLine($"Soma: {sum}");
+    }
+}
